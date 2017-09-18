771bf6906a51e93ff8dd02df5192f41123f46ab5
Pranav Sharma <psha7619@uni.sydney.edu.au>
2017-09-18 22:15:52 +1000

    More code cleanup

--- MediaAccess/app/src/main/java/comp5216/sydney/edu/au/mediaaccess/CameraActivity.java
+++ MediaAccess/app/src/main/java/comp5216/sydney/edu/au/mediaaccess/CameraActivity.java
@@ -1,7 +1,6 @@
 package comp5216.sydney.edu.au.mediaaccess;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
@@ -10,7 +9,6 @@ import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -24,9 +22,9 @@ import java.io.IOException;
  */
 public class CameraActivity extends Activity implements PictureCallback, SurfaceHolder.Callback {
 
-    public static final String EXTRA_CAMERA_DATA = "camera_data";
-
     private static final String KEY_IS_CAPTURING = "is_capturing";
+
+    private boolean hasCaptured = false;
 
     private Camera mCamera;
     private ImageView mCameraImage;
@@ -35,75 +33,93 @@ public class CameraActivity extends Activity implements PictureCallback, Surface
     private byte[] mCameraData;
     private boolean mIsCapturing;
 
-    private OnClickListener mCaptureImageButtonClickListener = new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            captureImage();
-        }
-    };
-
-    private OnClickListener mRecaptureImageButtonClickListener = new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            setupImageCapture();
-        }
-    };
-
-    private OnClickListener mDoneButtonClickListener = new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            if (mCameraData != null) {
-
-                Bitmap bmp = BitmapFactory.decodeByteArray(mCameraData, 0, mCameraData.length);
-                String photoPath = getIntent().getStringExtra("photoPath");
-
-                FileOutputStream fs = null;
-                try {
-                    fs = new FileOutputStream(photoPath);
-                    bmp.compress(Bitmap.CompressFormat.JPEG, 100, fs);
-                } catch (FileNotFoundException ex) {
-                    System.out.println(ex.getMessage());
-                } finally {
-                    try {
-                        fs.close();
-                    } catch (IOException ex) {
-                        System.out.println(ex.getMessage());
-                    }
-                }
-
-//                Toast.makeText(this, "Picture saved!", Toast.LENGTH_SHORT).show();
-
-                Intent intent = new Intent();
-                intent.putExtra(EXTRA_CAMERA_DATA, mCameraData);
-                setResult(RESULT_OK, intent);
-            } else {
-                setResult(RESULT_CANCELED);
-            }
-            finish();
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_camera);
 
-        mCameraImage = (ImageView) findViewById(R.id.camera_image_view);
+        mCameraImage = findViewById(R.id.camera_image_view);
         mCameraImage.setVisibility(View.INVISIBLE);
 
-        mCameraPreview = (SurfaceView) findViewById(R.id.preview_view);
+        mCameraPreview = findViewById(R.id.preview_view);
         final SurfaceHolder surfaceHolder = mCameraPreview.getHolder();
         surfaceHolder.addCallback(this);
-        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-
-        mCaptureImageButton = (Button) findViewById(R.id.capture);
-        mCaptureImageButton.setOnClickListener(mCaptureImageButtonClickListener);
-
-        final Button doneButton = (Button) findViewById(R.id.save);
-        doneButton.setOnClickListener(mDoneButtonClickListener);
 
         mIsCapturing = true;
+    }
+
+    public void onCapture(View v) {
+
+        if (!hasCaptured) {
+            // not captured
+            captureImage();
+        } else {
+            //captured
+            setupImageCapture();
+        }
+    }
+
+    private void captureImage() {
+        mCamera.takePicture(null, null, this);
+    }
+
+    @Override
+    public void onPictureTaken(byte[] data, Camera camera) {
+        mCameraData = data;
+        setupImageDisplay();
+    }
+
+    private void setupImageDisplay() {
+        Bitmap bitmap = BitmapFactory.decodeByteArray(mCameraData, 0, mCameraData.length);
+        mCameraImage.setImageBitmap(bitmap);
+        mCamera.stopPreview();
+        mCameraPreview.setVisibility(View.INVISIBLE);
+        mCameraImage.setVisibility(View.VISIBLE);
+        hasCaptured = true;
+    }
+
+    private void setupImageCapture() {
+        mCameraImage.setVisibility(View.INVISIBLE);
+        mCameraPreview.setVisibility(View.VISIBLE);
+        mCamera.startPreview();
+        hasCaptured = false;
+    }
+
+    public void onSave(View v) {
+        if (mCameraData != null) {
+
+            Bitmap bmp = BitmapFactory.decodeByteArray(mCameraData, 0, mCameraData.length);
+            String photoPath = getIntent().getStringExtra("photoPath");
+
+            FileOutputStream fs = null;
+            try {
+                fs = new FileOutputStream(photoPath);
+                bmp.compress(Bitmap.CompressFormat.JPEG, 100, fs);
+            } catch (FileNotFoundException ex) {
+                System.out.println(ex.getMessage());
+            } finally {
+                try {
+                    fs.close();
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+
+            Toast.makeText(this, "Picture was saved!",
+                    Toast.LENGTH_SHORT).show();
+
+            setResult(RESULT_OK);
+            finish();
+        } else {
+            Toast.makeText(this, "Nothing to save",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onQuit(View view) {
+        setResult(RESULT_CANCELED);
+        finish();
     }
 
     @Override
@@ -154,12 +170,6 @@ public class CameraActivity extends Activity implements PictureCallback, Surface
     }
 
     @Override
-    public void onPictureTaken(byte[] data, Camera camera) {
-        mCameraData = data;
-        setupImageDisplay();
-    }
-
-    @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
         if (mCamera != null) {
             try {
@@ -179,30 +189,5 @@ public class CameraActivity extends Activity implements PictureCallback, Surface
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-    }
-
-    private void captureImage() {
-        mCamera.takePicture(null, null, this);
-    }
-
-    private void setupImageCapture() {
-        mCameraImage.setVisibility(View.INVISIBLE);
-        mCameraPreview.setVisibility(View.VISIBLE);
-        mCamera.startPreview();
-        mCaptureImageButton.setText("Capture");
-        mCaptureImageButton.setOnClickListener(mCaptureImageButtonClickListener);
-    }
-
-    private void setupImageDisplay() {
-        Bitmap bitmap = BitmapFactory.decodeByteArray(mCameraData, 0, mCameraData.length);
-        mCameraImage.setImageBitmap(bitmap);
-        mCamera.stopPreview();
-        mCameraPreview.setVisibility(View.INVISIBLE);
-        mCameraImage.setVisibility(View.VISIBLE);
-        mCaptureImageButton.setOnClickListener(mRecaptureImageButtonClickListener);
-    }
-    public void onQuit(View view) {
-        setResult(RESULT_CANCELED);
-        finish();
     }
 }
